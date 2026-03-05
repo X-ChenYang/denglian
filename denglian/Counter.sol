a0f1db15e69b2bf9c31f6c7934e2aedaa6b8384b
X-ChenYang <214853224@qq.com>
2026-03-05 17:53:54 +0800

    1

---
+++ denglian/Counter.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+    
+    constructor() {
+        counter = 0;
+    }
+    
+    function get() public view returns (uint) {
+        return counter;
+    }
+    
+    function add(uint x) public {
+        counter += x;
+    }
+}
